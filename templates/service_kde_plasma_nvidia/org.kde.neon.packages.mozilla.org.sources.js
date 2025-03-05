fcd1d2df3dd937e5ce4f84f7e7fe438b8da0ecfb
Dan Mons <dan@mons.com.au>
2025-03-05 06:53:56 +0000

    KDE Neon

---
+++ templates/service_kde_plasma_nvidia/org.kde.neon.packages.mozilla.org.sources.js
@@ -0,0 +1,6 @@
+X-Repolib-Name: KDE neon org.kde.neon.packages.mozilla.org
+Types: deb
+URIs: https://packages.mozilla.org/apt
+Suites: mozilla
+Components: main
+Signed-By: /etc/apt/keyrings/org.kde.neon.packages.mozilla.org.asc
